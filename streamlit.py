2f4354cefc5d601eea5e462c26557a28f38c91c8
mahmoudjichi <145796705+mahmoudjichi@users.noreply.github.com>
2023-09-25 14:38:43 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,8 +4,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 st.title("Car Prices Dataset")
 st.write("This dataset provides a comprehensive collection of specifications and pricing details for a diverse range of cars. It serves as a valuable resource for automotive enthusiasts, researchers, and industry professionals looking to analyze and understand the intricate relationships between various car attributes and their market prices.")
+url = "https://raw.githubusercontent.com/mahmoudjichi/Assignment/main/carprices.csv"
 def load_data(nrows):
-    data = pd.read_csv("C:/Users/dell/OneDrive/Desktop/MSBA 325/carprices.csv", nrows=nrows)
+    data = pd.read_csv(url, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
