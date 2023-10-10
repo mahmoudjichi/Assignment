d50da199a4059aea128bf1e2f1fabd21da44ddeb
mahmoudjichi <145796705+mahmoudjichi@users.noreply.github.com>
2023-10-10 23:05:13 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -18,28 +18,28 @@ if st.checkbox('Show raw data'):
 data['torque (lb-ft)'] = pd.to_numeric(data['torque (lb-ft)'], errors='coerce')
 data['engine size (l)'] = pd.to_numeric(data['engine size (l)'], errors='coerce')
 data.dropna(subset=['torque (lb-ft)', 'engine size (l)'], inplace=True)
-st.write("""This bar chart below represents the performance of different car models based on their torque values. 
+st.write("""**This bar chart below represents the performance of different car models based on their torque values. 
 Torque is a measure of the force that can cause an object to rotate about an axis. 
-In the context of cars, it indicates the power of the engine and its ability to do work.""")
+In the context of cars, it indicates the power of the engine and its ability to do work.**""")
 min_torque, max_torque = int(data['torque (lb-ft)'].min()), int(data['torque (lb-ft)'].max())
 torque_range = st.slider("Select Torque Range", min_torque, max_torque, (min_torque, max_torque))
 filtered_data = data[(data['torque (lb-ft)'] >= torque_range[0]) & (data['torque (lb-ft)'] <= torque_range[1])]
 fig = px.bar(filtered_data, x="car model", y='torque (lb-ft)')
 st.subheader(f"Bar Chart for Cars with Torque between {torque_range[0]} and {torque_range[1]} lb-ft")
 st.plotly_chart(fig)
-st.write("""This box plot below visualizes the distribution of car prices for different car makes. 
+st.write("""**This box plot below visualizes the distribution of car prices for different car makes. 
 The box represents the interquartile range (IQR), showing the middle 50% of the data. 
 The whiskers extend to the minimum and maximum values within 1.5 times the IQR. 
-Outliers may be plotted as individual points outside the whiskers.""")
+Outliers may be plotted as individual points outside the whiskers.**""")
 selected_make = st.selectbox("Select Car Make", data['car make'].unique())
 filtered_data_make = data[data['car make'] == selected_make]
 fig1 = px.box(filtered_data_make, x="car make", y="price (in usd)")
 st.subheader(f"Price of {selected_make} Cars")
 st.plotly_chart(fig1)
-st.write("""This histogram below displays the frequency distribution of engine sizes for the cars in the dataset. 
+st.write("""**This histogram below displays the frequency distribution of engine sizes for the cars in the dataset. 
 The x-axis represents different engine sizes (in liters), while the y-axis shows the number of cars 
 that fall within each engine size range. This visualization helps understand the most common engine sizes 
-in the dataset.""")
+in the dataset.**""")
 min_engine, max_engine = float(data['engine size (l)'].min()), float(data['engine size (l)'].max())
 engine_range = st.slider("Select Engine Size Range", min_engine, max_engine, (min_engine, max_engine))
 filtered_data_engine = data[(data['engine size (l)'] >= engine_range[0]) & (data['engine size (l)'] <= engine_range[1])]
